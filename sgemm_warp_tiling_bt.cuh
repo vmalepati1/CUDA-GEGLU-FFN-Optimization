bf8765e93c73019ea0775435dee7bcd9170cdc35
Vikas Malepati <malepativikas@gmail.com>
2026-01-18 17:49:34 -0500

    Start work on ffn fused kernel

---
+++ sgemm_warp_tiling_bt.cuh
@@ -0,0 +1,204 @@
+#ifndef SGEMM_WARP_TILING_BT_H
+#define SGEMM_WARP_TILING_BT_H
+
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
+#include <iostream>
+#include <random>
+#include <cmath>
+
+#define WARPSIZE 32
+
+namespace wt {
+template <const int BM, const int BN, const int BK, const int rowStrideA,
+          const int rowStrideB>
+__device__ void loadFromGmem(int N, int K, const float *__restrict__ A, 
+                             const float *__restrict__ B,
+                             float *__restrict__ As, float *__restrict__ Bs, 
+                             int innerRowA, int innerColA,
+                             int innerRowB, int innerColB) {
+  for (unsigned int offset = 0; offset <= BM - rowStrideA; offset += rowStrideA) {
+    const float4 tmp = reinterpret_cast<const float4 *>(
+        &A[(innerRowA + offset) * K + innerColA * 4])[0];
+
+    // This causes a bunch of bank conflicts since we are writing along a column of As in SMEM
+    As[(innerColA * 4 + 0) * BM + innerRowA + offset] = tmp.x;
+    As[(innerColA * 4 + 1) * BM + innerRowA + offset] = tmp.y;
+    As[(innerColA * 4 + 2) * BM + innerRowA + offset] = tmp.z;
+    As[(innerColA * 4 + 3) * BM + innerRowA + offset] = tmp.w;
+  }
+
+  for (unsigned int offset = 0; offset <= BN - rowStrideB; offset += rowStrideB) {
+    const float4 tmp = reinterpret_cast<const float4 *>(
+        &B[(innerRowB + offset) * K + innerColB * 4])[0];
+
+    // This causes a bunch of bank conflicts since we are writing along a column of Bs in SMEM
+    Bs[(innerColB * 4 + 0) * BN + innerRowB + offset] = tmp.x;
+    Bs[(innerColB * 4 + 1) * BN + innerRowB + offset] = tmp.y;
+    Bs[(innerColB * 4 + 2) * BN + innerRowB + offset] = tmp.z;
+    Bs[(innerColB * 4 + 3) * BN + innerRowB + offset] = tmp.w;
+  }
+}
+
+template <const int BM, const int BN, const int BK, const int WM, const int WN,
+          const int WMITER, const int WNITER, const int WSUBM, const int WSUBN,
+          const int TM, const int TN>
+__device__ void
+processFromSmem(float *__restrict__ regM, float *__restrict__ regN, 
+                float *__restrict__ threadResults, 
+                const float *__restrict__ As,
+                const float *__restrict__ Bs, 
+                const unsigned int warpRow, const unsigned int warpCol,
+                const unsigned int threadRowInWarp, const unsigned int threadColInWarp) {
+  for (unsigned int dotIdx = 0; dotIdx < BK; ++dotIdx) {
+    // populate registers for whole warptile
+    #pragma unroll
+    for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
+
+      #pragma unroll
+      for (unsigned int i = 0; i < TM; ++i) {
+        regM[wSubRowIdx * TM + i] =
+            As[(dotIdx * BM) + warpRow * WM + wSubRowIdx * WSUBM +
+               threadRowInWarp * TM + i];
+      }
+    }
+
+    #pragma unroll
+    for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
+
+      #pragma unroll
+      for (unsigned int i = 0; i < TN; ++i) {
+        regN[wSubColIdx * TN + i] =
+            Bs[(dotIdx * BN) + warpCol * WN + wSubColIdx * WSUBN +
+               threadColInWarp * TN + i];
+      }
+    }
+
+    // execute warptile matmul
+
+    #pragma unroll
+    for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
+
+      #pragma unroll
+      for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
+        // calculate per-thread results
+
+        #pragma unroll
+        for (unsigned int resIdxM = 0; resIdxM < TM; ++resIdxM) {
+
+          #pragma unroll
+          for (unsigned int resIdxN = 0; resIdxN < TN; ++resIdxN) {
+            threadResults[(wSubRowIdx * TM + resIdxM) * (WNITER * TN) +
+                          (wSubColIdx * TN) + resIdxN] +=
+                regM[wSubRowIdx * TM + resIdxM] *
+                regN[wSubColIdx * TN + resIdxN];
+          }
+        }
+      }
+    }
+  }
+}
+
+} // namespace wt
+
+/*
+ * @tparam BM The threadblock size for M dimension SMEM caching.
+ * @tparam BN The threadblock size for N dimension SMEM caching.
+ * @tparam BK The threadblock size for K dimension SMEM caching.
+ * @tparam WM M dim of continuous tile computed by each warp
+ * @tparam WN N dim of continuous tile computed by each warp
+ * @tparam WMITER The number of subwarp tiling steps in M dimension.
+ * @tparam WNITER The number of subwarp tiling steps in N dimension.
+ * @tparam TM The per-thread tile size for M dimension.
+ * @tparam TN The per-thread tile size for N dimension.
+ */
+template <const int BM, const int BN, const int BK, const int WM, const int WN,
+          const int WNITER, const int TM, const int TN, const int NUM_THREADS>
+__global__ void __launch_bounds__(NUM_THREADS)
+    sgemmWarptiling_BT(int M, int N, int K,
+                    const float *__restrict__ A, 
+                    const float *__restrict__ B,
+                    float *__restrict__ C) {
+  const unsigned int cRow = blockIdx.y;
+  const unsigned int cCol = blockIdx.x;
+
+  // Placement of the warp in the threadblock tile
+  const unsigned int warpIdx = threadIdx.x / WARPSIZE; // the warp this thread is in
+  const unsigned int warpCol = warpIdx % (BN / WN);
+  const unsigned int warpRow = warpIdx / (BN / WN);
+
+  // size of the warp subtile
+  constexpr unsigned int WMITER = (WM * WN) / (WARPSIZE * TM * TN * WNITER);
+  constexpr unsigned int WSUBM = WM / WMITER; // 64/2=32
+  constexpr unsigned int WSUBN = WN / WNITER; // 32/2=16
+
+  // Placement of the thread in the warp subtile
+  const unsigned int threadIdxInWarp = threadIdx.x % WARPSIZE;         // [0, 31]
+  const unsigned int threadColInWarp = threadIdxInWarp % (WSUBN / TN); // i%(16/4)
+  const unsigned int threadRowInWarp = threadIdxInWarp / (WSUBN / TN); // i/4
+
+  // allocate space for the current blocktile in SMEM
+  __shared__ float As[BM * BK];
+  __shared__ float Bs[BK * BN];
+
+  // Move blocktile to beginning of A's row and B's column
+  A += cRow * BM * K;
+  B += cCol * BN * K;
+  // Move C_ptr to warp's output tile
+  C += (cRow * BM + warpRow * WM) * N + cCol * BN + warpCol * WN;
+
+  // calculating the indices that this thread will load into SMEM
+  // we'll load 128bit / 32bit = 4 elements per thread at each step
+  const unsigned int innerRowA = threadIdx.x / (BK / 4);
+  const unsigned int innerColA = threadIdx.x % (BK / 4);
+  constexpr unsigned int rowStrideA = (NUM_THREADS * 4) / BK;
+  const unsigned int innerRowB = threadIdx.x / (BK / 4);
+  const unsigned int innerColB = threadIdx.x % (BK / 4);
+  constexpr unsigned int rowStrideB = (NUM_THREADS * 4) / BK;
+
+  // allocate thread-local cache for results in registerfile
+  float threadResults[WMITER * TM * WNITER * TN] = {0.0};
+  // we cache into registers on the warptile level
+  float regM[WMITER * TM] = {0.0};
+  float regN[WNITER * TN] = {0.0};
+
+  // outer-most loop over block tiles
+  for (unsigned int bkIdx = 0; bkIdx < K; bkIdx += BK) {
+    wt::loadFromGmem<BM, BN, BK, rowStrideA, rowStrideB>(
+        N, K, A, B, As, Bs, innerRowA, innerColA, innerRowB, innerColB);
+    __syncthreads();
+    wt::processFromSmem<BM, BN, BK, WM, WN, WMITER, WNITER, WSUBM, WSUBN, TM,
+                        TN>(regM, regN, threadResults, As, Bs, warpRow, warpCol,
+                            threadRowInWarp, threadColInWarp);
+    A += BK;     // move BK columns to right
+    B += BK;     // move BK columns to right
+    __syncthreads();
+  }
+
+  // write out the results
+  for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
+    for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
+      // move C pointer to current warp subtile
+      float *C_interim = C + (wSubRowIdx * WSUBM) * N + wSubColIdx * WSUBN;
+      for (unsigned int resIdxM = 0; resIdxM < TM; resIdxM += 1) {
+        for (unsigned int resIdxN = 0; resIdxN < TN; resIdxN += 4) {
+
+          // perform GEMM update in reg
+          const int i = (wSubRowIdx * TM + resIdxM) * (WNITER * TN) +
+                        wSubColIdx * TN + resIdxN;
+
+          reinterpret_cast<float4 *>(
+            &C_interim[(threadRowInWarp * TM + resIdxM) * N +
+                       threadColInWarp * TN + resIdxN])[0] = 
+            reinterpret_cast<float4 *>(&threadResults[i])[0];
+        }
+      }
+    }
+  }
+}
+
+#endif /* SGEMM_WARP_TILING_BT_H */
--- simple_test_warp_tiled.cu
+++ simple_test_warp_tiled.cu
@@ -8,185 +8,12 @@
 #include <random>
 #include <cmath>
 
+#include "sgemm_warp_tiling_bt.cuh"
+
 #define CEIL_DIV(M, N) (((M) + (N)-1) / (N))
-const int WARPSIZE = 32; // warpSize is not constexpr
 
-#define WARMUP_ITERS 10
-#define BENCH_ITERS 50
-
-namespace wt {
-template <const int BM, const int BN, const int BK, const int rowStrideA,
-          const int rowStrideB>
-__device__ void loadFromGmem(int N, int K, const float *__restrict__ A, 
-                             const float *__restrict__ B,
-                             float *__restrict__ As, float *__restrict__ Bs, 
-                             int innerRowA, int innerColA,
-                             int innerRowB, int innerColB) {
-  for (unsigned int offset = 0; offset <= BM - rowStrideA; offset += rowStrideA) {
-    const float4 tmp = reinterpret_cast<const float4 *>(
-        &A[(innerRowA + offset) * K + innerColA * 4])[0];
-    As[(innerColA * 4 + 0) * BM + innerRowA + offset] = tmp.x;
-    As[(innerColA * 4 + 1) * BM + innerRowA + offset] = tmp.y;
-    As[(innerColA * 4 + 2) * BM + innerRowA + offset] = tmp.z;
-    As[(innerColA * 4 + 3) * BM + innerRowA + offset] = tmp.w;
-  }
-
-  for (unsigned int offset = 0; offset <= BK - rowStrideB; offset += rowStrideB) {
-    reinterpret_cast<float4 *>(
-        &Bs[(innerRowB + offset) * BN + innerColB * 4])[0] =
-        reinterpret_cast<const float4 *>(
-            &B[(innerRowB + offset) * N + innerColB * 4])[0];
-  }
-}
-
-template <const int BM, const int BN, const int BK, const int WM, const int WN,
-          const int WMITER, const int WNITER, const int WSUBM, const int WSUBN,
-          const int TM, const int TN>
-__device__ void
-processFromSmem(float *__restrict__ regM, float *__restrict__ regN, 
-                float *__restrict__ threadResults, 
-                const float *__restrict__ As,
-                const float *__restrict__ Bs, 
-                const unsigned int warpRow, const unsigned int warpCol,
-                const unsigned int threadRowInWarp, const unsigned int threadColInWarp) {
-  for (unsigned int dotIdx = 0; dotIdx < BK; ++dotIdx) {
-    // populate registers for whole warptile
-    for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
-      for (unsigned int i = 0; i < TM; ++i) {
-        regM[wSubRowIdx * TM + i] =
-            As[(dotIdx * BM) + warpRow * WM + wSubRowIdx * WSUBM +
-               threadRowInWarp * TM + i];
-      }
-    }
-    for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
-      for (unsigned int i = 0; i < TN; ++i) {
-        regN[wSubColIdx * TN + i] =
-            Bs[(dotIdx * BN) + warpCol * WN + wSubColIdx * WSUBN +
-               threadColInWarp * TN + i];
-      }
-    }
-
-    // execute warptile matmul
-    for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
-      for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
-        // calculate per-thread results
-        for (unsigned int resIdxM = 0; resIdxM < TM; ++resIdxM) {
-          for (unsigned int resIdxN = 0; resIdxN < TN; ++resIdxN) {
-            threadResults[(wSubRowIdx * TM + resIdxM) * (WNITER * TN) +
-                          (wSubColIdx * TN) + resIdxN] +=
-                regM[wSubRowIdx * TM + resIdxM] *
-                regN[wSubColIdx * TN + resIdxN];
-          }
-        }
-      }
-    }
-  }
-}
-
-} // namespace wt
-
-/*
- * @tparam BM The threadblock size for M dimension SMEM caching.
- * @tparam BN The threadblock size for N dimension SMEM caching.
- * @tparam BK The threadblock size for K dimension SMEM caching.
- * @tparam WM M dim of continuous tile computed by each warp
- * @tparam WN N dim of continuous tile computed by each warp
- * @tparam WMITER The number of subwarp tiling steps in M dimension.
- * @tparam WNITER The number of subwarp tiling steps in N dimension.
- * @tparam TM The per-thread tile size for M dimension.
- * @tparam TN The per-thread tile size for N dimension.
- */
-template <const int BM, const int BN, const int BK, const int WM, const int WN,
-          const int WNITER, const int TM, const int TN, const int NUM_THREADS>
-__global__ void __launch_bounds__(NUM_THREADS)
-    sgemmWarptiling(int M, int N, int K, float alpha, 
-                    const float *__restrict__ A, 
-                    const float *__restrict__ B,
-                    float beta, 
-                    float *__restrict__ C) {
-  const unsigned int cRow = blockIdx.y;
-  const unsigned int cCol = blockIdx.x;
-
-  // Placement of the warp in the threadblock tile
-  const unsigned int warpIdx = threadIdx.x / WARPSIZE; // the warp this thread is in
-  const unsigned int warpCol = warpIdx % (BN / WN);
-  const unsigned int warpRow = warpIdx / (BN / WN);
-
-  // size of the warp subtile
-  constexpr unsigned int WMITER = (WM * WN) / (WARPSIZE * TM * TN * WNITER);
-  constexpr unsigned int WSUBM = WM / WMITER; // 64/2=32
-  constexpr unsigned int WSUBN = WN / WNITER; // 32/2=16
-
-  // Placement of the thread in the warp subtile
-  const unsigned int threadIdxInWarp = threadIdx.x % WARPSIZE;         // [0, 31]
-  const unsigned int threadColInWarp = threadIdxInWarp % (WSUBN / TN); // i%(16/4)
-  const unsigned int threadRowInWarp = threadIdxInWarp / (WSUBN / TN); // i/4
-
-  // allocate space for the current blocktile in SMEM
-  __shared__ float As[BM * BK];
-  __shared__ float Bs[BK * BN];
-
-  // Move blocktile to beginning of A's row and B's column
-  A += cRow * BM * K;
-  B += cCol * BN;
-  // Move C_ptr to warp's output tile
-  C += (cRow * BM + warpRow * WM) * N + cCol * BN + warpCol * WN;
-
-  // calculating the indices that this thread will load into SMEM
-  // we'll load 128bit / 32bit = 4 elements per thread at each step
-  const unsigned int innerRowA = threadIdx.x / (BK / 4);
-  const unsigned int innerColA = threadIdx.x % (BK / 4);
-  constexpr unsigned int rowStrideA = (NUM_THREADS * 4) / BK;
-  const unsigned int innerRowB = threadIdx.x / (BN / 4);
-  const unsigned int innerColB = threadIdx.x % (BN / 4);
-  constexpr unsigned int rowStrideB = NUM_THREADS / (BN / 4);
-
-  // allocate thread-local cache for results in registerfile
-  float threadResults[WMITER * TM * WNITER * TN] = {0.0};
-  // we cache into registers on the warptile level
-  float regM[WMITER * TM] = {0.0};
-  float regN[WNITER * TN] = {0.0};
-
-  // outer-most loop over block tiles
-  for (unsigned int bkIdx = 0; bkIdx < K; bkIdx += BK) {
-    wt::loadFromGmem<BM, BN, BK, rowStrideA, rowStrideB>(
-        N, K, A, B, As, Bs, innerRowA, innerColA, innerRowB, innerColB);
-    __syncthreads();
-    wt::processFromSmem<BM, BN, BK, WM, WN, WMITER, WNITER, WSUBM, WSUBN, TM,
-                        TN>(regM, regN, threadResults, As, Bs, warpRow, warpCol,
-                            threadRowInWarp, threadColInWarp);
-    A += BK;     // move BK columns to right
-    B += BK * N; // move BK rows down
-    __syncthreads();
-  }
-
-  // write out the results
-  for (unsigned int wSubRowIdx = 0; wSubRowIdx < WMITER; ++wSubRowIdx) {
-    for (unsigned int wSubColIdx = 0; wSubColIdx < WNITER; ++wSubColIdx) {
-      // move C pointer to current warp subtile
-      float *C_interim = C + (wSubRowIdx * WSUBM) * N + wSubColIdx * WSUBN;
-      for (unsigned int resIdxM = 0; resIdxM < TM; resIdxM += 1) {
-        for (unsigned int resIdxN = 0; resIdxN < TN; resIdxN += 4) {
-          // load C vector into registers
-          float4 tmp = reinterpret_cast<float4 *>(
-              &C_interim[(threadRowInWarp * TM + resIdxM) * N +
-                         threadColInWarp * TN + resIdxN])[0];
-          // perform GEMM update in reg
-          const int i = (wSubRowIdx * TM + resIdxM) * (WNITER * TN) +
-                        wSubColIdx * TN + resIdxN;
-          tmp.x = alpha * threadResults[i + 0] + beta * tmp.x;
-          tmp.y = alpha * threadResults[i + 1] + beta * tmp.y;
-          tmp.z = alpha * threadResults[i + 2] + beta * tmp.z;
-          tmp.w = alpha * threadResults[i + 3] + beta * tmp.w;
-          // write back
-          reinterpret_cast<float4 *>(
-              &C_interim[(threadRowInWarp * TM + resIdxM) * N +
-                         threadColInWarp * TN + resIdxN])[0] = tmp;
-        }
-      }
-    }
-  }
-}
+#define WARMUP_ITERS 1
+#define BENCH_ITERS 1
 
 float *compareKernelAndCUBLAS(int m, int k, int n)
 {
@@ -218,25 +45,25 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
 
     /* GOOD PARAMETERS */
     /*
-        const unsigned int K10_NUM_THREADS = 32;
-        const unsigned int K10_BM = 32;
-        const unsigned int K10_BN = 128;
-        const unsigned int K10_BK = 16;
-        const unsigned int K10_WM = 32;
-        const unsigned int K10_WN = 128;
-        const unsigned int K10_WNITER = 4;
-        const unsigned int K10_TM = 2;
-        const unsigned int K10_TN = 4;
+      const unsigned int K10_NUM_THREADS = 256;
+      const unsigned int K10_BM = 32;
+      const unsigned int K10_BN = 128;
+      const unsigned int K10_BK = 32;
+      const unsigned int K10_WM = 16;
+      const unsigned int K10_WN = 32;
+      const unsigned int K10_WNITER = 2;
+      const unsigned int K10_TM = 2;
+      const unsigned int K10_TN = 4;
     */
 
     // B = 32
-    const unsigned int K10_NUM_THREADS = 32;
+    const unsigned int K10_NUM_THREADS = 256;
     const unsigned int K10_BM = 32;
     const unsigned int K10_BN = 128;
-    const unsigned int K10_BK = 16;
-    const unsigned int K10_WM = 32;
-    const unsigned int K10_WN = 128;
-    const unsigned int K10_WNITER = 4;
+    const unsigned int K10_BK = 32;
+    const unsigned int K10_WM = 16;
+    const unsigned int K10_WN = 32;
+    const unsigned int K10_WNITER = 2;
     const unsigned int K10_TM = 2;
     const unsigned int K10_TN = 4;
 
@@ -286,17 +113,18 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
     // Warm-up
     // --------------------
     for (int i = 0; i < WARMUP_ITERS; i++) {
-        sgemmWarptiling<K10_BM, K10_BN, K10_BK, K10_WM, K10_WN, K10_WNITER, K10_TM,
+        sgemmWarptiling_BT<K10_BM, K10_BN, K10_BK, K10_WM, K10_WN, K10_WNITER, K10_TM,
                   K10_TN, K10_NUM_THREADS>
-          <<<gridDim, blockDim>>>(m, n, k, alpha, dA, dBT, beta, dC);
+          <<<gridDim, blockDim>>>(m, n, k, dA, dBT, dC);
     }
 
+    // Compute C^T = B^T * A^T
     for (int i = 0; i < WARMUP_ITERS; i++) {
         cublasSgemm(handle,
                     CUBLAS_OP_T, CUBLAS_OP_N,
                     n, m, k,
                     &alpha,
-                    dBT, n,
+                    dBT, k,
                     dA, k,
                     &beta,
                     dC, n);
@@ -313,9 +141,9 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
     // --------------------
     cudaEventRecord(start);
     for (int i = 0; i < BENCH_ITERS; i++) {
-        sgemmWarptiling<K10_BM, K10_BN, K10_BK, K10_WM, K10_WN, K10_WNITER, K10_TM,
+        sgemmWarptiling_BT<K10_BM, K10_BN, K10_BK, K10_WM, K10_WN, K10_WNITER, K10_TM,
                   K10_TN, K10_NUM_THREADS>
-          <<<gridDim, blockDim>>>(m, n, k, alpha, dA, dBT, beta, dC);
+          <<<gridDim, blockDim>>>(m, n, k, dA, dBT, dC);
     }
     cudaEventRecord(end);
     cudaEventSynchronize(end);
@@ -335,7 +163,7 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
                     CUBLAS_OP_T, CUBLAS_OP_N,
                     n, m, k,
                     &alpha,
-                    dBT, n,
+                    dBT, k,
                     dA, k,
                     &beta,
                     dC, n);
@@ -374,12 +202,12 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
     }
 
     cudaFree(dA);
-    cudaFree(dB);
+    cudaFree(dBT);
     cudaFree(dC);
     cublasDestroy(handle);
 
     delete[] hA;
-    delete[] hB;
+    delete[] hBT;
     delete[] hC;
     delete[] hCRef;
 
@@ -388,7 +216,7 @@ float *compareKernelAndCUBLAS(int m, int k, int n)
 
 int main()
 {
-    int m = 32;
+    int m = 4;
     int k = 4096;
     int n = 12288;
 
